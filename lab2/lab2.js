e010acca0042d10998087d3ab50f64cb981c1537
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-05-19 08:10:43 -0500

    Update lab2.js
    change made by 2nd developer

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -3,3 +3,5 @@
 console.log("Practice for Push");
 // this is change made by other developer
 console.log("Change made by developer");
+//this is another change made by other developer
+console.log("....@@@.......")
